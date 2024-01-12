42295791186f365becc6b2c979a8f038e1c55240
MohamadElgendy23 <mohamadelgendy13@gmail.com>
2024-01-11 21:52:07 -0700

    adding routes mainly and controllers

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,6 +1,6 @@
 import app from "./app.js";
 import { connectToDatabase } from "./db/connection.js";
-
 connectToDatabase().then(() => {
     app.listen(5000, () => console.log("Server listening on port 5000"));
 }).catch((err) => console.log(err));
+//# sourceMappingURL=index.js.map
