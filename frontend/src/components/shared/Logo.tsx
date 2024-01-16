5871b01bee8ca142e6f96409c444f06d99323e00
MohamadElgendy23 <mohamadelgendy13@gmail.com>
2024-01-16 15:08:54 -0700

    currently added the navigation links such as logging in and signing up to the header

--- frontend/src/components/shared/Logo.tsx
+++ frontend/src/components/shared/Logo.tsx
@@ -8,8 +8,8 @@ function Logo() {
         <Link to={"/"}>
             <img src="openai.png" alt="openai" width={"30px"} height={"30px"} className="image-inverted" />
         </Link>
-        <Typography sx={{ display: {md: "block", sm: "none", xs: "none"}, mr: "auto", fontWeight: "800", textShadow: "2px 2px 20px #000", }}>
-            <span style={{ fontSize: "20px" }}>MERN</span>-GPT
+        <Typography sx={{ display: {md: "block", sm: "none", xs: "none"}, mr: "auto", fontWeight: "850", textShadow: "2px 2px 20px #000", }}>
+            <span style={{ fontSize: "25px" }}>MERN</span>-AI-GPT
         </Typography>
     </div>
   )
