5881cf03548012952541b62c563fe9a3798388cc
MohamadElgendy23 <mohamadelgendy13@gmail.com>
2024-01-17 16:05:27 -0700

    adding login functionality and its corresponding api client

--- frontend/src/components/shared/CustomizedInput.tsx
+++ frontend/src/components/shared/CustomizedInput.tsx
@@ -6,9 +6,8 @@ type Props = {
     type: string;
     label: string;
 }
-
 function CustomizedInput(props: Props) {
   return <TextField margin="normal" InputLabelProps={{ style: {color: "white"} }} name={props.name} label={props.label} type={props.type} InputProps={{ style: { width: "400px", borderRadius: 10, fontSize: 20, color: "white" }}}></TextField>
 }
 
-export default CustomizedInput
+export default CustomizedInput;
